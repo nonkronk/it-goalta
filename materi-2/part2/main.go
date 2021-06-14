b6aa1dc49eef748d1859c01cb0ee650df39ba7b9
nonkronk <nonkronkblues@gmail.com>
2021-06-14 20:44:58 +0700

    Variable & Data Type

---
+++ materi-2/part2/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var islogin bool = false
+	fmt.Println(islogin)
+
+	var prime int = 7
+	fmt.Println(prime)
+
+	var halo string = "hello"
+	fmt.Println(halo)
+
+	const pi = 3.14
+	fmt.Println(pi)
+
+}
