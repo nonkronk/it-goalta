7b367ed7b596b329f05adc28212a495e64bca575
nonkronk <nonkronkblues@gmail.com>
2021-06-19 20:12:27 +0700

    Create an API that parse two int, type, then return area of it. type: square, rectangle, triangle, parallelogram (use a method and a function)

--- materi-5/areaCalculator/main.go
+++ materi-5/areaCalculator/main.go
@@ -8,70 +8,90 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
+	"log"
 	"net/http"
 	"strconv"
 )
 
-type shape struct {
-	Base, Height float64
-	Type         string
+type Shape struct {
+	Base   float64
+	Height float64
+	Type   string
 }
 
 // Run the web server
 func main() {
-	http.HandleFunc("/areas", areas)
-	fmt.Println("Starting RESTFul API endpoint at http://localhost:8080/areas") // try curl http://localhost:8080/areas?base=10&height=10&type=rectangle
-	http.ListenAndServe(":8080", nil)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/areas", areas)
+	log.Println("Starting RESTFul API endpoint at http://localhost:8080/areas") // try curl http://localhost:8080/areas?base=10&height=10&type=rectangle
+	err := http.ListenAndServe(":8080", mux)
+	log.Fatal(err)
 }
 
 func areas(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	// Check "base" parameter
-	base, err := strconv.ParseFloat(r.FormValue("base"), 64)
+	// Setup decoder and return error for any ignored or not matching key-value
+	dec := json.NewDecoder(r.Body)
+	dec.DisallowUnknownFields()
+
+	// Decode json into struct
+	var s Shape
+	err := dec.Decode(&s)
 	if err != nil {
-		http.Error(w, `Invalid "base" parameter`, http.StatusBadRequest)
-		return
+		// Enable request by URL
+		// If API accesed without body
+		if err.Error() == "EOF" {
+			// Check "base" parameter
+			base, err := strconv.ParseFloat(r.FormValue("base"), 64)
+			if err != nil {
+				http.Error(w, `Invalid "base" parameter`, http.StatusBadRequest)
+				return
+			}
+			// Check "height" parameter
+			height, err := strconv.ParseFloat(r.FormValue("height"), 64)
+			if err != nil {
+				http.Error(w, `Invalid "height" parameter`, http.StatusBadRequest)
+				return
+			}
+			// Check "type" parameter
+			type_ := r.FormValue("type")
+			if !(type_ == "square" || type_ == "rectangle" || type_ == "triangle" || type_ == "parallelogram") {
+				http.Error(w, `Invalid "type" parameter`, http.StatusBadRequest)
+				return
+			}
+			s.Base = base
+			s.Height = height
+			s.Type = type_
+		} else {
+			log.Println(err.Error())
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
 	}
-	// Check "height" parameter
-	height, err := strconv.ParseFloat(r.FormValue("height"), 64)
-	if err != nil {
-		http.Error(w, `Invalid "height" parameter`, http.StatusBadRequest)
-		return
-	}
-	// Check "type" parameter
-	type_ := r.FormValue("type")
-	if !(type_ == "square" || type_ == "rectangle" || type_ == "triangle" || type_ == "parallelogram") {
-		http.Error(w, `Invalid "type" parameter`, http.StatusBadRequest)
-		return
-	}
-	// An idiomatic way to encapsulate new struct creation
-	s := newShape(base, height, type_)
 
 	if r.Method == "GET" {
-		// Parse calculated area into a map
-		area := map[string]float64{"result": s.area()}
-		// Encode area result into json
-		result, _ := json.Marshal(area)
-		w.Write(result)
-		return
+		// Identify unrecognized value of key "type"
+		if s.area() < 0 {
+			log.Println(`Invalid "type" parameter`)
+			http.Error(w, `Invalid "type" parameter`, http.StatusBadRequest)
+			return
+		} else {
+			// Populate calculated area into a map
+			area := map[string]float64{"result": s.area()}
+			// Encode area result into json
+			result, _ := json.Marshal(area)
+			w.Write(result)
+			return
+		}
 	}
 	http.Error(w, "", http.StatusBadRequest)
 }
 
-// Construct a new shape struct with the given name
-func newShape(base float64, height float64, type_ string) *shape {
-	s := shape{Type: type_}
-	s.Base = base
-	s.Height = height
-	return &s
-}
-
 // Calculate area method
-func (s shape) area() float64 {
+func (s Shape) area() float64 {
 	if s.Type == "square" || s.Type == "rectangle" || s.Type == "parallelogram" {
 		return s.Base * s.Height
-	} else {
+	} else if s.Type == "triangle" {
 		return s.Base * s.Height / 2
 	}
+	return -1
 }
