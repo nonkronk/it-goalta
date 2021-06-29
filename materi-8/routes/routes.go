88d4ec90346ccedc70177557299427aa2b745f53
nonkronk <nonkronkblues@gmail.com>
2021-06-30 02:58:00 +0700

    Add groups of handling based on authorization level

--- materi-8/routes/routes.go
+++ materi-8/routes/routes.go
@@ -1,62 +1,77 @@
 package routes
 
 import (
+	"net/http"
 	"project/controllers"
+	"project/middlewares"
+	"project/models"
 
 	"github.com/labstack/echo"
 )
 
+// Initialize echo router
 func New() *echo.Echo {
 	e := echo.New()
+	// Root
+	e.GET("/", rootHandler)
 
+	// Auth routes
+	authGroup := e.Group("")
+	// Create new user & admin
+	authGroup.POST("/register", controllers.CreateUsersController)
+	// Login the user and get the access token
+	authGroup.POST("/login", controllers.LoginUsersController)
+	// Create refresh token handler (BONUS)
+	authGroup.POST("/refresh", controllers.RefreshTokenController, middlewares.AuthenticationMiddleware(), middlewares.AuthorizationMiddleware("refresh"))
+
+	// Admin Group
+	adminGroup := e.Group("")
+	// Use authentication and admin-level authorization
+	adminGroup.Use(middlewares.AuthenticationMiddleware(), middlewares.AuthorizationMiddleware("basicAdminPermissions"))
+	adminGroup.GET("/users", controllers.GetAllUsersController)
+	adminGroup.GET("/user/:id", controllers.GetUserController)
 	// Customers routes
-	e.POST("/customer", controllers.CreateCustomerController)
-	// Pagination has been implemented
-	// The default per_page size is 10
-	//
-	// e.g. We can also use query  -->  /customers?per_page=5&page=2
-	//
-	// It will return the items from index 6 to 10 (index >= 10)
-	e.GET("/customers", controllers.GetAllCustomersController)
-	// Filter by id
-	e.GET("/customer/:id", controllers.GetCustomerController)
-	// Update & delete by id
-	e.PUT("/customer/:id", controllers.UpdateCustomerController)
-	e.DELETE("/customer/:id", controllers.DeleteCustomerController)
-
+	adminGroup.POST("/customer", controllers.CreateCustomerController)
+	adminGroup.GET("/customers", controllers.GetAllCustomersController)
+	adminGroup.GET("/customer/:id", controllers.GetCustomerController)
+	adminGroup.PUT("/customer/:id", controllers.UpdateCustomerController)
+	adminGroup.DELETE("/customer/:id", controllers.DeleteCustomerController)
 	// Car routes
-	e.POST("/car", controllers.CreateCarController)
-	// Pagination has been implemented
-	// The default per_page size is 10
-	e.GET("/cars", controllers.GetAllCarsController)
-	// Filter by id
-	e.GET("/car/:id", controllers.GetCarController)
-	// Update & delete by id
-	e.PUT("/car/:id", controllers.UpdateCarController)
-	e.DELETE("/car/:id", controllers.DeleteCarController)
-
+	adminGroup.POST("/car", controllers.CreateCarController)
+	adminGroup.GET("/cars", controllers.GetAllCarsController)
+	adminGroup.GET("/car/:id", controllers.GetCarController)
+	adminGroup.PUT("/car/:id", controllers.UpdateCarController)
+	adminGroup.DELETE("/car/:id", controllers.DeleteCarController)
 	// Garage routes
-	e.POST("/garage", controllers.CreateGarageController)
-	// Pagination has been implemented
-	// The default per_page size is 10
-	e.GET("/garages", controllers.GetAllGaragesController)
-	// Filter by id
-	e.GET("/garage/:id", controllers.GetGarageController)
-	// Update & delete by id
-	e.PUT("/garage/:id", controllers.UpdateGarageController)
-	e.DELETE("/garage/:id", controllers.DeleteGarageController)
-
+	adminGroup.POST("/garage", controllers.CreateGarageController)
+	adminGroup.GET("/garages", controllers.GetAllGaragesController)
+	adminGroup.GET("/garage/:id", controllers.GetGarageController)
+	adminGroup.PUT("/garage/:id", controllers.UpdateGarageController)
+	adminGroup.DELETE("/garage/:id", controllers.DeleteGarageController)
 	// Order routes
-	// Request order
-	e.POST("/order", controllers.CreateOrderController)
-	// Get all the orders data optionally filtered by query params
-	e.GET("/orders", controllers.GetAllOrdersController)
-	// Filter by id
-	e.GET("/order/:id", controllers.GetOrderController)
-	// Close order
-	e.POST("/order/done", controllers.CloseOrderController)
-
+	adminGroup.POST("/order", controllers.CreateOrderController)
+	adminGroup.GET("/orders", controllers.GetAllOrdersController)
+	adminGroup.GET("/order/:id", controllers.GetOrderController)
+	adminGroup.POST("/order/done", controllers.CloseOrderController)
 	// Get all orders data based on the struct/database structure (BONUS)
 	e.GET("/orders/v2", controllers.GetAllOrdersControllerV2)
+
+	// Users Group (employees) only have acces to order api and read cars.
+	userGroup := e.Group("")
+	// Use authentification and user-level authorization
+	userGroup.Use(middlewares.AuthenticationMiddleware(), middlewares.AuthorizationMiddleware("refresh"))
+	userGroup.GET("/cars", controllers.GetAllCarsController)
+	userGroup.GET("/car/:id", controllers.GetCarController)
+	// Order routes
+	userGroup.POST("/order", controllers.CreateOrderController)
+	userGroup.GET("/orders", controllers.GetAllOrdersController)
+	userGroup.GET("/order/:id", controllers.GetOrderController)
+	userGroup.POST("/order/done", controllers.CloseOrderController)
+
 	return e
+}
+
+// A functional easter-egg!
+func rootHandler(c echo.Context) error {
+	return c.JSON(http.StatusOK, models.Response{Message: "IT works!"})
 }
